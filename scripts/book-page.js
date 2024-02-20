578f2874413f3a0a676d07f1279b2f85a7df9561
Soooooox <Q2820854831@gmail.com>
2024-02-20 21:35:43 +0800

    update book-cover show
    更新了书的阴影、添加了书籍舒痕，改变了背景为纸张纹理

--- scripts/book-page.js
+++ scripts/book-page.js
@@ -93,33 +93,25 @@ homebutton.addEventListener(
 function createCard(element){
 				const div_row = document.createElement('div');
         div_row.setAttribute('class', 'row');
-		    div_row.setAttribute('id', 'row');
 
         const div_column = document.createElement('div');
         div_column.setAttribute('class', 'column');
 
         const div_card = document.createElement('div');
         div_card.setAttribute('class', 'book-card');
-		    div_card.setAttribute('id', 'book-card');
 
-        const center = document.createElement('center');
+        const book_cover = document.createElement('div');
+				book_cover.setAttribute('class', 'book-cover');
 
         const image = document.createElement('img');
         image.setAttribute('class', 'book-img');
-        image.setAttribute('id', 'img');
-
-        const title = document.createElement('h2');
-        title.setAttribute('class', 'book-title');
-
-        title.innerHTML = `${element.title}`;
-        image.src = IMG_PATH + element.img_path;
+				image.src = IMG_PATH + element.img_path;  
 
         container.appendChild(div_row);
         div_row.appendChild(div_column);
         div_column.appendChild(div_card);
-        div_card.appendChild(center);
-        center.appendChild(image);
-        div_card.appendChild(title);
+        div_card.appendChild(book_cover);
+        book_cover.appendChild(image);
 				return div_card;
 }
 // 刷新页面
